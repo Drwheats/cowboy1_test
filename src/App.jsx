652b8c7c7dc1b32ec30524fea43a932d70d1efb5
Gondola <Dr.Wheats@gmail.com>
2022-05-04 16:08:47 -0400

    test1

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navigatorbar from './components/Navigatorbar';
+import './App.css';
+import Home from "./pages/Home";
+import Social from "./pages/Social";
+import Login from "./pages/Login";
+import {BrowserRouter, Routes, Route, NavLink, Navigate} from "react-router-dom";
+
+
+const App = () => {
+    const user = true;
+    return (
+    <BrowserRouter>
+      <div>
+        <Navigatorbar user={user} />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+                path="/login"
+                element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route path="/social/:id" element={user ? <Social/> : <Navigate to="login"/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    );
+};
+
+export default App;
